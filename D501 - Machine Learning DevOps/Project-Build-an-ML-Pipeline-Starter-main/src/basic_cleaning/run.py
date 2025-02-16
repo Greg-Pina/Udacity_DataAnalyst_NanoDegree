fdd758a40945708002a49fa20ea6832483da6ae3
Greg Pina <gpina6@wgu.edu>
2025-02-16 03:50:00 -0500

    attempting to run data_check

--- D501 - Machine Learning DevOps/Project-Build-an-ML-Pipeline-Starter-main/src/basic_cleaning/run.py
+++ D501 - Machine Learning DevOps/Project-Build-an-ML-Pipeline-Starter-main/src/basic_cleaning/run.py
@@ -14,12 +14,10 @@ logger = logging.getLogger()
 # DO NOT MODIFY
 def go(args):
 
-    run = wandb.init(job_type="basic_cleaning")
+    run = wandb.init(project="nyc_airbnb", group="cleaning", job_type="basic_cleaning", save_code=True)
     run.config.update(args)
 
     # Download input artifact. This will also log that this script is using this
-    
-    run = wandb.init(project="nyc_airbnb", group="cleaning", save_code=True)
     artifact_local_path = run.use_artifact(args.input_artifact).file()
     df = pd.read_csv(artifact_local_path)
     # Drop outliers
@@ -53,44 +51,44 @@ if __name__ == "__main__":
   
     parser.add_argument(
         "--input_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type=str,
+        help="Name of the input artifact",
+        required=True
     )
 
     parser.add_argument(
         "--output_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type=str,
+        help="The name for the output artifact",
+        required=True
     )
 
     parser.add_argument(
         "--output_type", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type=str,
+        help="The type for the output artifact",
+        required=True
     )
 
     parser.add_argument(
         "--output_description", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type=str,
+        help="A description of the output artifact",
+        required=True
     )
 
     parser.add_argument(
         "--min_price", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type=float,
+        help="The minimum price to consider",
+        required=True
     )
 
     parser.add_argument(
         "--max_price",
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type=float,
+        help="The maximum price to consider",
+        required=True
     )
 
 
--- D501 - Machine Learning DevOps/Project-Build-an-ML-Pipeline-Starter-main/src/data_check/conftest.py
+++ D501 - Machine Learning DevOps/Project-Build-an-ML-Pipeline-Starter-main/src/data_check/conftest.py
@@ -1,7 +1,6 @@
 import pytest
-import pandas as pd
 import wandb
-
+import pandas as pd
 
 def pytest_addoption(parser):
     parser.addoption("--csv", action="store")
@@ -9,7 +8,6 @@ def pytest_addoption(parser):
     parser.addoption("--kl_threshold", action="store")
     parser.addoption("--min_price", action="store")
     parser.addoption("--max_price", action="store")
-
 
 @pytest.fixture(scope='session')
 def data(request):
@@ -26,46 +24,28 @@ def data(request):
 
     return df
 
-
 @pytest.fixture(scope='session')
 def ref_data(request):
     run = wandb.init(job_type="data_tests", resume=True)
 
-    # Download input artifact. This will also note that this script is using this
-    # particular version of the artifact
-    data_path = run.use_artifact(request.config.option.ref).file()
+    # Download reference artifact
+    ref_data_path = run.use_artifact(request.config.option.ref).file()
 
-    if data_path is None:
+    if ref_data_path is None:
         pytest.fail("You must provide the --ref option on the command line")
 
-    df = pd.read_csv(data_path)
+    ref_df = pd.read_csv(ref_data_path)
 
-    return df
-
+    return ref_df
 
 @pytest.fixture(scope='session')
 def kl_threshold(request):
-    kl_threshold = request.config.option.kl_threshold
-
-    if kl_threshold is None:
-        pytest.fail("You must provide a threshold for the KL test")
-
-    return float(kl_threshold)
+    return float(request.config.option.kl_threshold)
 
 @pytest.fixture(scope='session')
 def min_price(request):
-    min_price = request.config.option.min_price
-
-    if min_price is None:
-        pytest.fail("You must provide min_price")
-
-    return float(min_price)
+    return float(request.config.option.min_price)
 
 @pytest.fixture(scope='session')
 def max_price(request):
-    max_price = request.config.option.max_price
-
-    if max_price is None:
-        pytest.fail("You must provide max_price")
-
-    return float(max_price)
+    return float(request.config.option.max_price)
